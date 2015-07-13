cb4acba9a17a727f65c6a7ba4ee3dd391235ce89
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2015-07-13 21:42:34 +0200

    Countdown stops when reach the 0

--- src/entities/game-elements/timer.js
+++ src/entities/game-elements/timer.js
@@ -2,7 +2,7 @@ play.createTimer = function(){
 		var timer = this.time.create(false);
 		timer.left = this.level.time;
 		timer.updateTime = updateTime;
-		timer.loop(1000,timer.updateTime.bind(timer));
+		timer.timeLeftEvent = timer.loop(1000,timer.updateTime.bind(timer));
 		return timer;
 }
 
@@ -11,6 +11,9 @@ function updateTime(){
 
 		this.left--;
 		//TODO gameover if timeLeft == 0
+		if (this.left === 0){
+			this.remove(this.timeLeftEvent);//does it work ?
+		}
 		//Update text timer
 		play.timer.text.count.setText(this.left);
 		//TIMER BASED EVENTS
